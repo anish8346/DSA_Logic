7b43ecee43dc7300849ea8c954fe89491f59ea2c
Anish <kumaranish68346@gmail.com>
2026-05-24 17:31:36 +0530

    Added Que_07 solution

---
+++ phase_01/Que_07.java
@@ -0,0 +1,23 @@
+package phase_01;
+import java.util.Scanner;
+public class Que_07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the Second Number : ");
+        int b = sc.nextInt();
+        System.out.print("Enter the Third Number : ");
+        int c = sc.nextInt();
+
+        if (a>b && a > c) {
+            System.out.println(a);
+        }
+        else if (b > a && b > c) {
+            System.out.println(b);
+        }
+        else {
+            System.out.println(c);
+        }
+    }
+}
